c22671e99a5e42a46283cef2a855b165eba09d9e
vitaly <bakeinflash@gmail.com>
2021-05-01 19:43:27 +0300

    disabling cff_driver_class for switch

--- include/freetype/config/ftmodule.h
+++ include/freetype/config/ftmodule.h
@@ -13,7 +13,9 @@
 FT_USE_MODULE( FT_Module_Class, autofit_module_class )
 FT_USE_MODULE( FT_Driver_ClassRec, tt_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, t1_driver_class )
-FT_USE_MODULE( FT_Driver_ClassRec, cff_driver_class )
+#if !defined(NINTENDO_LIB)
+ FT_USE_MODULE( FT_Driver_ClassRec, cff_driver_class )
+#endif
 FT_USE_MODULE( FT_Driver_ClassRec, t1cid_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, pfr_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, t42_driver_class )
